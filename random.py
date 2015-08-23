4a76744a39708fa64a2fb2864e27945c6da65943
Hattwick <philhattwick@charter.net>
2015-08-23 09:18:49 -0400

    new file

---
+++ random.py
@@ -0,0 +1,26 @@
+# Random number test
+
+import random
+
+secretNumber = random.randint(1,100)
+
+print('What is the random number between 1 and 100?')
+
+# Take six guesses
+for guessesTaken in range(1,7):
+    print('Take a guess:')
+    guess = int(input())
+
+    if guess < secretNumber:
+        print ('Guess is low.')
+    elif guess > secretNumber:
+        print ('Guess is high.')
+    else:
+        break    # guess was correct
+
+    if guess == secretNumber:
+        print('You guessed the number correctly. ' + str(guessesTaken) + ' guesses used.')
+    else:
+        print ('Failed.  The correct number was ' + str(secretNumber))
+
+# end 
