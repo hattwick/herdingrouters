4837663a9196bb3c0e97e7be46533e139ea4a0c7
Hattwick <philhattwick@charter.net>
2015-09-05 23:21:49 -0400

    pylint pass 1

--- teambuilder.py
+++ teambuilder.py
@@ -6,6 +6,7 @@ import random, sys, os, math
 # Initiate empty list
 teammate = []
 
+
 # Loop to gather team members
 while True:
     print('Type the name of a team member ' + str(len(teammate) + 1) + ' and press enter.  Enter nothing and press Enter to stop: ')
@@ -14,25 +15,30 @@ while True:
        break
     teammate = teammate + [name]
 
+
 # Print list when finished entering
 print('The team is:')
 for name in teammate:
     print('  ' + name)
 
+
 # Iterate slightly differently
 for i in range(len(teammate)):
    print ('Index ' + str(i) + ' in teammates list is: ' + teammate[i])
+
 
 # Sort the iterate again
 teammate.sort()
 for i in range(len(teammate)):
    print ('Index ' + str(i) + ' in sorted teammates list is: ' + teammate[i])
 
+
 # Reverse the sort
 teammate.sort(reverse=True)
 for i in range(len(teammate)):
    print ('Index ' + str(i) + ' in teammates Reverse Sort list is: ' + teammate[i])
    
+
 # Find index position of team member
 while True:
     print('If there is someone you want to search for, Type the name here.  Enter nothing to finish without searching.')
